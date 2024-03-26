b80200d328b4b73709603e3feca2c86ea598d2f4
viet363 <110683284+viet363@users.noreply.github.com>
2024-03-26 21:41:03 +0700

    Update Home.jsx

--- src/Compoment/Home.jsx
+++ src/Compoment/Home.jsx
@@ -2,6 +2,259 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div class='w-full h-[60px] flex justify-between items-center bg-[#DCFFEC] py-3 px-6'>
+        <div>
+          <p>Phòng Đào Tạo </p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="tăng" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center py-3 px-6 border-b-4 border-black'>
+        <div className=''>
+          <p>Thành viên A </p>
+        </div>
+        <div class='flex justify-center items-center'>
+          <p>Chức vụ</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="Tăng" />
+          </button>
+          <button>
+            <img src="./icon/trash-solid.svg" alt="Xóa" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center py-3 px-6 border-b-4 border-black'>
+        <div>
+          <p>Thành viên B </p>
+        </div>
+        <div class='flex justify-center items-center '>
+          <p>Chức vụ</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="Tăng" />
+          </button>
+          <button>
+            <img src="./icon/trash-solid.svg" alt="Xóa" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center bg-[#DCFFEC] py-3 px-6'>
+        <div>
+          <p>Phòng Công Tác Sinh Viên </p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="tăng" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center py-3 px-6 border-b-4 border-black'>
+        <div>
+          <p>Thành viên A </p>
+        </div>
+        <div class='flex justify-center items-center'>
+          <p>Chức vụ</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="Tăng" />
+          </button>
+          <button>
+            <img src="./icon/trash-solid.svg" alt="Xóa" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center py-3 px-6 border-b-4 border-black'>
+        <div>
+          <p>Thành viên B </p>
+        </div>
+        <div class='flex justify-center items-center'>
+          <p>Chức vụ</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="Tăng" />
+          </button>
+          <button>
+            <img src="./icon/trash-solid.svg" alt="Xóa" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center bg-[#DCFFEC] py-3 px-6'>
+        <div>
+          <p>Phòng Khảo Thí </p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="tăng" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center py-3 px-6 border-b-4 border-black'>
+        <div>
+          <p>Thành viên A </p>
+        </div>
+        <div class='flex justify-center items-center'>
+          <p>Chức vụ</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="Tăng" />
+          </button>
+          <button>
+            <img src="./icon/trash-solid.svg" alt="Xóa" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center py-3 px-6 border-b-4 border-black'>
+        <div>
+          <p>Thành viên B </p>
+        </div>
+        <div class='flex justify-center items-center'>
+          <p>Chức vụ</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="Tăng" />
+          </button>
+          <button>
+            <img src="./icon/trash-solid.svg" alt="Xóa" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center bg-[#DCFFEC] py-3 px-6'>
+        <div>
+          <p>Phòng Tài Chính - Quản Trị</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="tăng" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center py-3 px-6 border-b-4 border-black'>
+        <div>
+          <p>Thành viên A </p>
+        </div>
+        <div class='flex justify-center items-center'>
+          <p>Chức vụ</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="Tăng" />
+          </button>
+          <button>
+            <img src="./icon/trash-solid.svg" alt="Xóa" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center py-3 px-6 border-b-4 border-black'>
+        <div>
+          <p>Thành viên B </p>
+        </div>
+        <div class='flex justify-center items-center'>
+          <p>Chức vụ</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="Tăng" />
+          </button>
+          <button>
+            <img src="./icon/trash-solid.svg" alt="Xóa" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center bg-[#DCFFEC] py-3 px-6'>
+        <div>
+          <p>Phòng Kế Hoạch - Tài Chính </p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="tăng" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center py-3 px-6 border-b-4 border-black'>
+        <div>
+          <p>Thành viên A </p>
+        </div>
+        <div class='flex justify-center items-center'>
+          <p>Chức vụ</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="Tăng" />
+          </button>
+          <button>
+            <img src="./icon/trash-solid.svg" alt="Xóa" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center py-3 px-6 border-b-4 border-black'>
+        <div>
+          <p>Thành viên B </p>
+        </div>
+        <div class='flex justify-center items-center'>
+          <p>Chức vụ</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="Tăng" />
+          </button>
+          <button>
+            <img src="./icon/trash-solid.svg" alt="Xóa" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center bg-[#DCFFEC] py-3 px-6'>
+        <div>
+          <p>Đoàn Viên </p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="tăng" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center py-3 px-6 border-b-4 border-black'>
+        <div>
+          <p>Thành viên A </p>
+        </div>
+        <div class='flex justify-center items-center'>
+          <p>Chức vụ</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="Tăng" />
+          </button>
+          <button>
+            <img src="./icon/trash-solid.svg" alt="Xóa" />
+          </button>
+        </div>
+      </div>
+      <div class='w-full h-[60px] flex justify-between items-center py-3 px-6 border-b-4 border-black'>
+        <div>
+          <p>Thành viên B </p>
+        </div>
+        <div class='flex justify-center items-center'>
+          <p>Chức vụ</p>
+        </div>
+        <div class='flex gap-[30px] justify-center items-center'>
+          <button>
+            <img src="./icon/plus-solid.svg" alt="Tăng" />
+          </button>
+          <button>
+            <img src="./icon/trash-solid.svg" alt="Xóa" />
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
